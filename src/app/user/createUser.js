95f5cd1a6313c4d5429b17e951106b36ad7ee7fd
Douglas Marques <douglas.bmarques@hotmail.com>
2020-10-23 11:33:17 -0300

    Validation refactor
    unused loop removal

--- src/app/user/createUser.js
+++ src/app/user/createUser.js
@@ -8,25 +8,18 @@ function makeCreateUser({ userRepository, mailService }) {
 
         const user = new User({ name: name, email: email, cpf: cpf, age: age });
 
-        if (user.legalAge() && user.validCPF()) {
-            const newUser = await userRepository.addOne(user);
+        try {
+            if (user.validate()) {
+                const newUser = await userRepository.addOne(user);
 
-            const confirmationEmail = new ConfirmationEmail({ destination: user.email });
+                const confirmationEmail = new ConfirmationEmail({ destination: user.email });
 
-            await mailService.sendMail(confirmationEmail);
+                await mailService.sendMail(confirmationEmail);
 
-            return newUser;
-        }
-
-        else if (!user.validCPF()) {
-            const error = new Error("Invalid CPF")
-            error.status = 'INVALID_CPF'
-            throw error
-        }
-        else if (!user.legalAge()) {
-            const error = new Error("Invalid age")
-            error.status = 'INVALID_AGE'
-            throw error
+                return newUser;
+            }
+        } catch (error) {
+            throw error;
         }
     }
 }
